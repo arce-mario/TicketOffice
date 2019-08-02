a54d2444586cbf074da20993a7b657b62b763b71
Mario Arce <mario.arce4526@est.uca.edu.ni>
2019-08-02 01:42:06 -0600

    Creación del API, del cliente, configuración de la conexión, creación de la entidad Movie, CRUD de la entidad Movie

---
+++ ApiCatchFilms/Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ApiCatchFilms.Models
+{
+    public class Movie
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("movie_id")]
+        public int movieID {get; set;}
+        [Required]
+        [StringLength(30)]
+        public string name { get; set; }
+        [Required]
+        [StringLength(30)]
+        public String type { get; set; }
+        [Required]
+        [StringLength(500)]
+        public String description { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string classification { get; set; }
+        [Required]
+        public TimeSpan time { get; set; }
+    }
+}
