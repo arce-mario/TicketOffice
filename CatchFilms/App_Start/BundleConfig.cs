18bb608cef7da2475e96d233f0ce1a53ab61195f
yulissa99 <yulissap.h@gmail.com>
2019-08-03 00:53:08 -0600

    Implementación de la plantilla para el cliente web

---
+++ CatchFilms/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Optimization;
+
+namespace CatchFilms
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/css")
+                .Include(
+                "~/Content/fonts/font-awesome.min.css",
+                "~/Content/style.css"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/js")
+                .Include(
+                "~/Content/js/jquery-1.11.1.min.js",
+                "~/Content/js/plugins.js",
+                "~/Content/js/app.js"));
+
+        }
+    }
+}
