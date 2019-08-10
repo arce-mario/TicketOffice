f891c2d8ee104b148796dfb76552e53d0869cbc3
adrianasalome31 <adrianasalome2017@gmail.com>
2019-08-09 18:20:44 -0600

    Creacion del formulario para registrar una pelicula

--- CatchFilms/Models/Movie.cs
+++ CatchFilms/Models/Movie.cs
@@ -1,14 +1,26 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
 
 namespace CatchFilms.Models
 {
     public class Movie
     {
-        public int movieID {get; set;}
-        public string name { get; set; }        
-        public String type { get; set; }       
+        public int? movieID {get; set;}
+        [DisplayName("Nombre de la película")]
+        public string name { get; set; }
+        [DisplayName("Tipo")]
+        public String type { get; set; }
+        [DisplayName("Descripción")]
         public String description { get; set; }
-        public string classification { get; set; }        
+        [DisplayName("Categoría")]
+        public string classification { get; set; }
+        [DisplayName("Hora")]
         public TimeSpan time { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string coverURL { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string imageURL { get; set; }
+        public float raiting { get; set; }
     }
 }
