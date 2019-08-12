118408bb4e95eab6e0691ead9515eabbdd5fa17a
Mario Arce <mario.arce4526@est.uca.edu.ni>
2019-08-12 13:28:00 -0600

    Creacion de formularios de funciones y salas

---
+++ CatchFilms/Controllers/RoomController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CatchFilms.Controllers
+{
+    public class RoomController : Controller
+    {
+        public async Task<string> getRooms(string name = "")
+        {
+            using (var client = new HttpClient())
+            {
+                if (name != "")
+                {
+                    client.BaseAddress = new Uri(LoginController.BaseUrl);
+                    client.DefaultRequestHeaders.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage res = await client.GetAsync(String.Concat("api/rooms?name=", name));
+
+                    if (res.IsSuccessStatusCode)
+                    {
+                        return res.Content.ReadAsStringAsync().Result;
+                    }
+                }
+            }
+            return "[]";
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ CatchFilms/Models/CatalogType.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CatchFilms.Models
+{
+    public class CatalogType
+    {
+        public int typeID { get; set; }
+        public string description { get; set; }
+        private List<CatalogType> moviesTypes;
+        private List<CatalogType> functionsTypes;
+        public CatalogType() { }
+        public void init ()
+        {
+            functionsTypes = new List<CatalogType>();
+            moviesTypes = new List<CatalogType>();
+
+            //Tipos de funciones
+            functionsTypes.Add(new CatalogType() { typeID = 1, description = "Gran premier" });
+            functionsTypes.Add(new CatalogType() { typeID = 2, description = "Estreno" });
+            functionsTypes.Add(new CatalogType() { typeID = 3, description = "Próximamente" });
+
+            //Tipos de películas
+            moviesTypes.Add(new CatalogType() { typeID = 1, description = "2D" });
+            moviesTypes.Add(new CatalogType() { typeID = 2, description = "3D" });
+        }
+
+        public IEnumerable<SelectListItem> getFunctionsTypes
+        {
+            get { return new SelectList(functionsTypes, "typeID", "description"); }
+        }
+
+        public IEnumerable<SelectListItem> getMoviesTypes
+        {
+            get { return new SelectList(moviesTypes, "typeID", "description"); }
+        }
+    }
+}
